b737c35e4bf62a06ce227d86d291164ec9de01da
Andr3aL <aleo759291@gmail.com>
2025-02-17 16:49:30 +0100

    page our team probleme cards

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,17 @@
 // alert("je suis connecté");
 
+// let images = document.querySelector(".card-img");
+
+
+let images = document.getElementsByClassName('card-img');
+
+for (let i = 0; i < images.length; i++) {
+    images[i].addEventListener('mouseover', () => {
+        images[i].style.opacity = 1;
+    });
+
+    images[i].addEventListener('mouseout', () => {
+        images[i].style.opacity = 0;
+    });
+}
+
